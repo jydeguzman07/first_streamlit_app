b123e0e3751240cfad3534488301385e82e550c1
jydeguzman07 <128138570+jydeguzman07@users.noreply.github.com>
2023-03-22 18:08:45 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-from urllib.
+from urllib.error import URLError
 #import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
